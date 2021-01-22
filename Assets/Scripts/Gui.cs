2015b818a2d04213e348b4dea0dd85dc95b31186
arun m <arunkattungal@gmail.com>
2021-01-22 22:01:34 +0530

    Initial Commit Trampoline Simulation

---
+++ Assets/Scripts/Gui.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gui : MonoBehaviour
+{
+
+
+    public Image forceFillBar;
+    public GameObject startButton;
+    public GameObject pauseButton;
+    public GameObject resumeButton;
+
+
+    private void Start()
+    {
+        startButton.SetActive(true);
+        pauseButton.SetActive(false);
+        resumeButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+
+
+    public void StartGamePlay()
+    {
+        startButton.SetActive(false);
+        pauseButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void OnPause()
+    {
+        Time.timeScale = 0;
+        resumeButton.SetActive(true);
+        pauseButton.SetActive(false);
+    }
+    public void OnResume()
+    {
+        Time.timeScale = 1;
+        resumeButton.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,94 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Jump Force
+    [SerializeField]
+    private float force;
+
+    private Rigidbody rigidBody;
+
+    //Initial weght
+    [SerializeField]
+    private float weight;
+
+    //Additional weight add on every second
+    [SerializeField]
+    private float additionalWeight;
+
+    //Timer variable
+    private float timer;
+    private float initialWeight;
+    [SerializeField]
+    private float maxWeight;
+
+    private void Awake()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+        rigidBody.mass = weight;
+        initialWeight = weight;
+    }
+
+    private void Update()
+    {
+        //Timer to add Additional mass to rigidbody
+        timer += Time.deltaTime;
+        if (timer < 1)
+            return;
+        timer = 0;
+
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0))
+        {
+            weight = weight + additionalWeight;
+
+        }
+        else
+        {
+            weight = weight - additionalWeight;
+
+        }
+
+#elif UNITY_ANDROID 
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Stationary)
+            {
+                weight = weight + additionalWeight;
+            }
+        }
+        else
+        {
+            weight = weight - additionalWeight;
+        }
+#endif
+
+        //add mass to rigidbody
+        if (weight < initialWeight)
+            weight = initialWeight;
+        else if (weight > maxWeight)
+            weight = maxWeight;
+
+        rigidBody.mass = weight;
+
+    }
+
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+
+        //Check Against trampolin layer
+        if (other.gameObject.layer == 9)
+        {
+            //add force to upwards
+            rigidBody.velocity = Vector3.up*weight;
+        }
+    }
+
+
+}
